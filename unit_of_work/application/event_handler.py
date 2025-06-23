09002f3ced41a14b270d7cc2fc3d9a561550b1d4
OlehDziuba <oleg_dzuba@outlook.com>
2025-06-23 21:39:40 +0300

    refactor: Make generic variable T in 'EventHandler' class covariant

--- unit_of_work/application/event_handler.py
+++ unit_of_work/application/event_handler.py
@@ -5,7 +5,7 @@ from typing import TypeVar, Generic
 from unit_of_work.domain import DomainEvent
 
 
-T = TypeVar('T', bound=DomainEvent)
+T = TypeVar('T', bound=DomainEvent, covariant=True)
 
 
 class EventHandler(abc.ABC, Generic[T]):
