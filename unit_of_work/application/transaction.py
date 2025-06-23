b878e8612801f9e07503a990181e311a4127b054
OlehDziuba <oleg_dzuba@outlook.com>
2025-06-23 17:55:47 +0300

    feat: Introduce 'Transaction' interface

---
+++ unit_of_work/application/transaction.py
@@ -0,0 +1,11 @@
+import abc
+
+
+class Transaction(abc.ABC):
+    @abc.abstractmethod
+    async def commit(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def rollback(self) -> None:
+        raise NotImplementedError
