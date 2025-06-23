3d14ed8d7d423dc910fd7a76e88bbaec7dfec73c
OlehDziuba <oleg_dzuba@outlook.com>
2025-06-23 20:30:15 +0300

    feat: Introduce 'Entity' class

--- unit_of_work/__init__.py
+++ unit_of_work/__init__.py
@@ -1,2 +1,3 @@
 from .application import Transaction, TransactionalRepository, UnitOfWork
+from .domain import Entity
 from .infrastructure import AsyncpgTransactionAdapter
---
+++ unit_of_work/domain/entity.py
@@ -0,0 +1,23 @@
+import dataclasses
+from datetime import datetime, UTC
+from uuid import UUID
+
+
+@dataclasses.dataclass(frozen=True)
+class DomainEvent:
+    occurred_at: datetime
+
+
+class Entity:
+    def __init__(self):
+        self.__domain_events: list[DomainEvent] = []
+
+    def registrate_event(self, event: DomainEvent) -> None:
+        self.__domain_events.append(event)
+
+    def clear_events(self) -> None:
+        self.__domain_events.clear()
+
+    @property
+    def events(self) -> list[DomainEvent]:
+        return self.__domain_events
